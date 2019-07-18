ee6561f6a86c098bf64245e8f1fedc1596b53c5e
KekMadao <saatwikrk@gmail.com>
2019-07-18 14:07:44 +0200

    Basic Obstacle Spawner
    It's set up to have 4 different lanes and cubes the size of an entire lane are spawned and moved towards the camera, once they move behind the camera the cubes are destroyed. Took more inspiration from the bike game Jon showed us instead of the snake vs block game.

---
+++ Assets/Scripts/LevelSpawnScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSpawnScript : MonoBehaviour
+{
+    public GameObject trafficPrefab;
+    private GameObject trafficClone;
+
+    public float spawnTime;
+    private bool canSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame (Obstacles and Passengers can spawn at -1.5, -0.5, 0,5 or 1,5 for x and  due to size of road; z pos of spawn is 80 and y pos is  1.8)
+    void Update()
+    {
+        spawnTime += Time.deltaTime;
+
+        if (spawnTime > 2)
+        {
+            float xPos = 0.5f;
+            int rand = Random.Range(1, 5);
+
+            if(rand == 1)
+            {
+                xPos = -1.5f;
+            }
+            else 
+            if(rand == 2)
+            {
+                xPos = -0.5f; 
+            }
+            else
+                if(rand == 4)
+            {
+                xPos = 1.5f;
+            }
+
+            trafficClone = Instantiate(trafficPrefab, new Vector3(xPos, 1.88f, 80), trafficPrefab.transform.rotation);
+            spawnTime = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/TrafficScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficScript : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame; Makes the object move towards the camera, deletes object if it goes past the camera
+    void Update()
+    {
+        transform.position -= new Vector3(0,0,speed);
+
+        if(transform.position.z <= -14)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
