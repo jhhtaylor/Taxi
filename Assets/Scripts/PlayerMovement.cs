e940036dfbbdea316d72275961bc3aae1d16ec73
jhhtaylor <jhhtaylor@gmail.com>
2019-07-18 18:34:03 +0200

    Commit of player and movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameController GC;
+    public int laneNum;
+
+    void FixedUpdate()
+    {
+        if (!GC.isGameOver)
+        {
+            if (Input.GetKey(KeyCode.A) && laneNum > 0){
+
+            }
+        }
+    }
+}
