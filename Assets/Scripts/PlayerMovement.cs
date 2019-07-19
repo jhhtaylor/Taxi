b5052b6ad2cb169f2d3aee2cb8cab7a6869d32bf
jhhtaylor <jhhtaylor@gmail.com>
2019-07-19 10:32:53 +0200

    Added player movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public GameController GC;
-    public int laneNum;
+    public int laneNum = 2;
 
     void FixedUpdate()
     {
-        if (!GC.isGameOver)
+        if (true)
         {
-            if (Input.GetKey(KeyCode.A) && laneNum > 0){
+            if (Input.GetKeyDown(KeyCode.A) && laneNum > 1)
+            {
+                gameObject.transform.position -= new Vector3(1, 0, 0);
+                laneNum--;
+
+            }
+            if (Input.GetKeyDown(KeyCode.D) && laneNum < 4)
+            {
+                gameObject.transform.position += new Vector3(1, 0, 0);
+                laneNum++;
 
             }
         }
